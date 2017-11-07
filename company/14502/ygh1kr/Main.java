0e6760db7d8d878a27c2edb7fd156c252d3ec655
LeeGeunHAHAHA <ygh1kr@naver.com>
2017-11-07 11:46:28 +0900

    solve virus in java

--- company/14502/ygh1kr/Main.java
+++ company/14502/ygh1kr/Main.java
@@ -1,8 +1,8 @@
-package virus;
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -13,17 +13,10 @@ public class Main {
     public static int dy[] = {1,0,0,-1};
 
     public static void main(String args[]) throws Exception {
-        int zeros = getInput();
-        test();
-        for(int i =0; i<n; i++)
-            for(int j =0; j<m; j++) {
-                if(map[i][j]==2) {
-                    flood(i,j);
-                    System.out.println(i+" "+j);
-                }
-            }
-        out.write(zeros + "\n\n");
-        test();
+        getInput();
+        int score = buildWall(0);
+
+        out.write(score+ "");
         in.close();
         out.close();
     }
@@ -53,30 +46,52 @@ public class Main {
     }
 
 
-    public static void flood(int x, int y) {
+    public static void flood(int m[][],int x, int y) {
         int tx = x;
         int ty = y;
         for(int i =0; i< 4; i++) {
             tx = x+dx[i];
             ty =y+dy[i];
-            if (isInRange(tx,ty)&&map[tx][ty]==0) {
-                map[tx][ty]=3;
-                flood(tx,ty);
+            if (isInRange(tx,ty)&&m[tx][ty]==0) {
+                m[tx][ty]=3;
+                flood(m,tx,ty);
             }
         }
         return ;
     }
-    public void buildWall(int step,int x, int y){
-        if(step ==3){
-            for(int i =x; i<n; i++) {
-                for(int j=y; j<m; j++) {
-
+    public static int  calc(int map[][]) {
+        int cnt = 0;
+        int tmp[][] = new int[n][m];
+        for(int i =0; i<n; i++) {
+            tmp[i] = Arrays.copyOf(map[i], map[i].length);
+        }
+        for(int i =0; i<n; i++)
+            for(int j =0; j<m; j++) {
+                if(map[i][j]==2) {
+                    flood(tmp,i,j);
                 }
             }
+        for(int i=0;i<n; i++) {
+            for(int j =0; j<m; j++) {
+                if (tmp[i][j]== 0) cnt ++;
+            }
         }
+        return cnt;
+    }
+    public static int buildWall(int step){
+        if(step==3) return calc(map);
+        int i, j, score = -1;
+
+        for(i=0;i<n;i++)
+            for(j=0;j<m;j++)
+                if(map[i][j]==0){
+                    map[i][j] = 1;
+                    score = Math.max(buildWall(step+1), score);
+                    map[i][j] = 0;
+                }
+        return score; 
     }
     public static boolean isInRange(int x , int y) {
         return x>=0 && y >= 0 && x<n && y<m;
     }
 }
-
